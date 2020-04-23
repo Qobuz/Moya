4d8225d2f4bc7df2d69a35c5c7d8bff90de82459
Renaud Codarini <renaud.codarini@qobuz.com>
2020-04-23 16:06:11 +0200

    Package files lighten

--- Moya.podspec
+++ Moya.podspec
@@ -17,27 +17,14 @@ Pod::Spec.new do |s|
   s.osx.deployment_target = '10.10'
   s.tvos.deployment_target = '9.0'
   s.watchos.deployment_target = '3.0'
-  s.source       = { :git => "https://github.com/Moya/Moya.git", :tag => s.version }
+  s.source       = { :git => "https://github.com/Qobuz/Moya.git", :tag => s.version }
   s.default_subspec = "Core"
   s.swift_version = '5.0'
   s.cocoapods_version = '>= 1.4.0'  
 
   s.subspec "Core" do |ss|
     ss.source_files  = "Sources/Moya/", "Sources/Moya/Plugins/"
-    ss.dependency "Alamofire", "~> 4.1"
-    ss.dependency "Result", "~> 4.1"
+    ss.dependency "Alamofire", "~> 4.9.1"
     ss.framework  = "Foundation"
-  end
-
-  s.subspec "ReactiveSwift" do |ss|
-    ss.source_files = "Sources/ReactiveMoya/"
-    ss.dependency "Moya/Core"
-    ss.dependency "ReactiveSwift", "~> 6.0"
-  end
-
-  s.subspec "RxSwift" do |ss|
-    ss.source_files = "Sources/RxMoya/"
-    ss.dependency "Moya/Core"
-    ss.dependency "RxSwift", "~> 5.0"
   end
 end
--- Package.swift
+++ Package.swift
@@ -4,40 +4,16 @@ import PackageDescription
 let package = Package(
     name: "Moya",
     products: [
-        .library(name: "Moya", targets: ["Moya"]),
-        .library(name: "ReactiveMoya", targets: ["ReactiveMoya"]),
-        .library(name: "RxMoya", targets: ["RxMoya"])
+        .library(name: "Moya", targets: ["Moya"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/antitypical/Result.git", .upToNextMajor(from: "4.1.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: ("6.0.0"))),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.9.1"))
     ],
     targets: [
          .target(
             name: "Moya",
             dependencies: [
-                "Alamofire",
-                "Result"],
-            exclude: [
-                "Tests",
-                "Sources/Supporting Files",
-                "Examples"]),
-        .target(
-            name: "ReactiveMoya",
-            dependencies: [
-                "Moya",
-                "ReactiveSwift"],
-            exclude: [
-                "Tests",
-                "Sources/Supporting Files",
-                "Examples"]),
-        .target(
-            name: "RxMoya",
-            dependencies: [
-                "Moya",
-                "RxSwift"],
+                "Alamofire"],
             exclude: [
                 "Tests",
                 "Sources/Supporting Files",
